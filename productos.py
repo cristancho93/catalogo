83a436fa96cc97f37497a5c940f459317ee6d5e0
Cristian Roncancio <croncancio@etg.global>
2021-04-17 18:39:17 -0500

    Me devuelvo al codigo original

--- productos.py
+++ productos.py
@@ -36,8 +36,50 @@ class EscudoOrcos(Escudo):
         self.imagen = "static/imagenes/orcos/escudo.png"
         self.descripcion = "escudo orcos"
 
-
-
 class Montura(Producto):
     def __init__(self):
         Producto.__init__(self)
+
+
+class MonturaOrcos(Montura):
+    def __init__(self):
+        self.grupo = "Orcos"
+        self.imagen = "static/imagenes/orcos/montura.jpg"
+        self.descripcion = "montura orcos"
+
+
+class MonturaHumanos(Montura):
+    def __init__(self):
+        self.grupo = "Humanos"
+        self.imagen = "static/imagenes/humanos/montura.jpg"
+        self.descripcion = "montura humanos"
+
+#------------------------------------------------------------Fabrica 2------------------------------------
+class Cuerpo:
+    def __init__(self):
+        self.brazos = ""
+        self.piernas = ""
+        self.cabeza = ""
+        self.torso = ""
+        self.imgcuerpo = ""
+
+class Cuerpo1(Cuerpo):
+    def __init__(self):
+        Cuerpo.__init__(self)
+
+class CuerpoHumano(Cuerpo1):
+    def __init__(self):
+        self.brazos = "Fuertes"
+        self.piernas = "Largas"
+        self.cabeza = "Pequeña"
+        self.torso = "Grande"
+        self.imgcuerpo = "static/imagenes/humanos/Cuerpo.jpg"
+
+class CuerpoOrco(Cuerpo1):
+    def __init__(self):
+        self.brazos = "Feos"
+        self.piernas = "Feos"
+        self.cabeza = "Feos"
+        self.torso = "Feos"
+        self.imgcuerpo = "static/imagenes/orcos/Cuerpo.jpg"
+
